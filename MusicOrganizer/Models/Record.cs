f323549c0d40d844bb040c13a72a10c93040aab2
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-21 14:06:47 -0700

    pass test 1 to instantiate a record

--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -4,5 +4,12 @@ namespace MusicOrganizer.Models
 {
   public class Record
   {
+    public string RecordTitle { get; set; }
+    public int Id { get; }
+
+    public Record(string recordTitle)
+    {
+      RecordTitle = recordTitle;
+    }
   }
 }
